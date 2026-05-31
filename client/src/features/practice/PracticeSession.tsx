315dc04de929c707ab3a8a03675bed42056a6d3b
danielor101010 <77803609+danielor101010@users.noreply.github.com>
2026-05-31 10:47:28 +0300

    fix bug

---
+++ client/src/features/practice/PracticeSession.tsx
@@ -0,0 +1,38 @@
+import { useEffect, useRef } from 'react'
+import { useParams, useLocation } from 'react-router-dom'
+import { useSessionStore } from '../../stores/sessionStore'
+import { useChatStore } from '../../stores/chatStore'
+import { useGraphStore } from '../../stores/graphStore'
+import { useScoreStore } from '../../stores/scoreStore'
+import { useUserStore } from '../../stores/userStore'
+import { sendWS } from '../../lib/ws'
+import { SessionLayout } from '../../layouts/SessionLayout'
+
+export const PracticeSession = () => {
+  const { problemId } = useParams<{ problemId: string }>()
+  const location = useLocation()
+  const customProblem = location.state?.customProblem as { title: string; description: string } | undefined
+  const sessionMode = (location.state?.sessionMode ?? 'practice') as 'practice' | 'concept'
+  const { startSession } = useSessionStore()
+  const { clearMessages, closeSolution } = useChatStore()
+  const { clearGraph } = useGraphStore()
+  const { reset: resetScore } = useScoreStore()
+  const userLevel = useUserStore((s) => s.level)
+  const sessionCreated = useRef(false)
+
+  useEffect(() => {
+    if (!problemId || sessionCreated.current) return
+    sessionCreated.current = true
+    clearMessages()
+    clearGraph()
+    resetScore()
+    closeSolution()
+    startSession(sessionMode, problemId)
+    const timer = setTimeout(() => {
+      sendWS('CREATE_SESSION', { mode: sessionMode, problemId, userLevel, ...(customProblem ? { customProblem } : {}) })
+    }, 500)
+    return () => clearTimeout(timer)
+  }, [problemId])
+
+  return <SessionLayout mode="practice" />
+}
